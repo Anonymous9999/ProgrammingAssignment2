cd5e8d6be74df5c6b446ff67ab0cb326502c2f9d
Anonymous9999 <Anonymous9999@users.noreply.github.com>
2015-05-22 19:50:47 +0100

    initial version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there are benefits 
+## to caching the inverse rather than computing it repeatedly. 
+## The following pair of functions cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" that can cache its inverse.
+makeCacheMatrix <- function(data = matrix()) 
+{
+    cache <- NULL
+    
+    set <- function(value) 
+    {
+        data  <<- value
+        cache <<- NULL
+    }
+    get <- function() { return(data) }
+    
+    setCache <- function(value) { cache <<- value }
+    getCache <- function()      { return(cache)   }
+    
+    return(list(
+         set      = set
+        ,get      = get
+        ,setCache = setCache
+        ,getCache = getCache))  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function computes the inverse of the special "matrix" returned 
+## from makeCacheMatrix.
+cacheSolve <- function(x, ...) 
+{
+    ## If the value has not been cached then calculate the inverse and
+    ## set it as the new cache. The "dim" function returns a vector when 
+    ## given a matrix and returns NULL when given a NULL object. 
+    if (is.null(dim(x$getCache())))
+    {
+        x$setCache(solve(x$get(), ...))
+    }
+    ## Write a message if the value has been previously cached.
+    ## The last line of the function will return the cache so there is
+    ## no need to return it here.
+    else
+    {
+        message("Getting cached data")
+    }    
+    
+    return(x$getCache())
 }
